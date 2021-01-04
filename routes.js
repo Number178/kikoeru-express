79e6aa6d3b673c376330cc2762313a008117be95
mason1900 <mw2010a@gmail.com>
2021-01-03 22:14:11 -0500

    歌词API

--- routes.js
+++ routes.js
@@ -66,7 +66,7 @@ router.get('/stream/:id/:index', (req, res, next) => {
             const track = tracks[req.params.index];
 
             const fileName = path.join(rootFolder.path, work.dir, track.subtitle || '', track.title);
-            if (path.extname(fileName) === '.txt') {
+            if (path.extname(fileName) === '.txt' || path.extname(fileName) === '.lrc') {
               const fileBuffer = fs.readFileSync(fileName);
               const charsetMatch = jschardet.detect(fileBuffer).encoding;
               if (charsetMatch) {
@@ -94,6 +94,44 @@ router.get('/download/:id/:index', (req, res, next) => {
           .then((tracks) => {
             const track = tracks[req.params.index];
             res.download(path.join(rootFolder.path, work.dir, track.subtitle || '', track.title));
+          })
+          .catch(err => next(err));
+      } else {
+        res.status(500).send({error: `找不到文件夹: "${work.root_folder}"，请尝试重启服务器或重新扫描.`});
+      }
+    });
+});
+
+router.get('/check-lrc/:id/:index', (req, res, next) => {
+  db.knex('t_work')
+    .select('root_folder', 'dir')
+    .where('id', '=', req.params.id)
+    .first()
+    .then((work) => {
+      const rootFolder = config.rootFolders.find(rootFolder => rootFolder.name === work.root_folder);
+      if (rootFolder) {
+        getTrackList(req.params.id, path.join(rootFolder.path, work.dir))
+          .then((tracks) => {
+            const track = tracks[req.params.index];
+            const fileLoc = path.join(rootFolder.path, work.dir, track.subtitle || '', track.title);
+            const lrcFileLoc = fileLoc.substr(0, fileLoc.lastIndexOf(".")) + ".lrc";
+
+            if (!fs.existsSync(lrcFileLoc)) {
+              //console.log('不存在歌词文件');
+              res.send({result: false, message:'不存在歌词文件', hash: ''});
+            } else {
+              console.log('找到歌词文件');
+              
+              const lrcFileName = track.title.substr(0, track.title.lastIndexOf(".")) + ".lrc";
+              const subtitleToFind = track.subtitle;
+              console.log('歌词文件名： ', lrcFileName);
+              tracks.forEach(trackItem => {
+                if (trackItem.title === lrcFileName && subtitleToFind === trackItem.subtitle) {
+                    console.log(trackItem.hash);
+                    res.send({result: true, message:'找到歌词文件', hash: trackItem.hash});
+                }
+              })
+            }
           })
           .catch(err => next(err));
       } else {
