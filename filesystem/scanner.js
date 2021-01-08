dd8262022619f550e7bcf53cb2b093eaf812dc6d
mason1900 <mw2010a@gmail.com>
2021-01-08 05:19:55 -0500

    Undo error handling

--- filesystem/scanner.js
+++ filesystem/scanner.js
@@ -302,7 +302,7 @@ const processFolder = (folder) => db.knex('t_work')
           }
         });
     }
-  }).catch(err => console.error(err));
+  });
 
 const MAX = config.maxParallelism; // 并发请求上限
 const limitP = new LimitPromise(MAX); // 核心控制器
@@ -587,7 +587,7 @@ const performUpdate = () => db.knex('t_work').select('id')
           }
         });
     }
-  }).catch(err => console.error(err));
+  });
 
   // process.send({
   //   event: 'SCAN_ERROR',
