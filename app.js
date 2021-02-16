cf55513af5de119fa8eb0b48ea49667b0365ec30
mason1900 <mw2010a@gmail.com>
2021-02-16 05:31:27 -0500

    Refactor: split socket from app.js
    Hoist

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const path = require('path');
 const express = require('express');
+const initSocket = require('./socket');
 const compression = require('compression');
 const bodyParser = require('body-parser'); // 获取 req.body
 const history = require('connect-history-api-fallback');
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
-const jwtAuth = require('socketio-jwt-auth'); // 用于 JWT 验证的 socket.io 中间件
-const child_process = require('child_process'); // 子进程
-const { initApp }= require('./database/schema');
 
+const { initApp }= require('./database/schema');
 const { config } = require('./config');
 const api = require('./api');
-
 const app = express();
+
+// Initialize database if not exists 
+// Init or migrate database and config
+// Note: non-blocking
+initApp().catch(err => console.error(err));
 
 if (config.enableGzip) {
   app.use(compression());
@@ -40,6 +43,22 @@ api(app);
 // Serve WebApp routes
 app.use(express.static(path.join(__dirname, './dist')));
 
+// 返回错误响应
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') { 
+    // 验证错误  
+    res.status(401).send({ error: err.message });
+  } else if (err.code === 'SQLITE_ERROR') {
+    if (err.message.indexOf('no such table') !== -1) {
+      res.status(500).send({ error: '数据库结构尚未建立，请先执行扫描.'});
+    }
+  } else {
+    res.status(500).send({ error: err.message || err });
+  }
+});
+
+// Create HTTP and HTTPS server
 const server = http.createServer(app);
 let httpsServer = null;
 let httpsSuccess = false;
@@ -56,104 +75,11 @@ if (config.httpsEnabled) {
   }
 }
 
-function initSocket (io) {
-  if (config.auth) {
-    io.use(jwtAuth.authenticate({
-      secret: config.jwtsecret
-    }, (payload, done) => {
-      const user = {
-        name: payload.name,
-        group: payload.group
-      };
-
-      if (user.name === 'admin') {
-        done(null, user);
-      } else {
-        done(null, false, '只有 admin 账号能登录管理后台.');
-      }
-    }));
-  }
-
-  let scanner = null;
-
-  // 有新的客户端连接时触发
-  io.on('connection', function (socket) {
-    // console.log('connection');
-    socket.emit('success', {
-      message: '成功登录管理后台.',
-      user: socket.request.user,
-      auth: config.auth
-    });
-
-    // socket.on('disconnect', () => {
-    //   console.log('disconnect');
-    // });
-    
-    socket.on('ON_SCANNER_PAGE', () => {
-      if (scanner) {
-        // 防止用户在扫描过程中刷新页面
-        scanner.send({
-          emit: 'SCAN_INIT_STATE'
-        });
-      }
-    });
-
-    socket.on('PERFORM_SCAN', () => {
-      if (!scanner) {
-        scanner = child_process.fork(path.join(__dirname, './filesystem/scanner.js'), { silent: false }); // 子进程
-        scanner.on('exit', (code) => {
-          scanner = null;
-          if (code) {
-            io.emit('SCAN_ERROR');
-          }
-        });
-        
-        scanner.on('message', (m) => {
-          if (m.event) {
-            io.emit(m.event, m.payload);
-          }
-        });
-      }   
-    });
-
-    socket.on('KILL_SCAN_PROCESS', () => {
-      scanner.send({
-        exit: 1
-      });
-    });
-
-    // 发生错误时触发
-    socket.on('error', (err) => {
-      console.error(err);
-    });
-  });
-}
-
 // websocket 握手依赖 http 服务
-const io = require('socket.io')(server);
-initSocket(io);
-
+initSocket(server);
 if (config.httpsEnabled) {
-  const ioSecure = require('socket.io')(httpsServer);
-  initSocket(ioSecure);
+  initSocket(httpsServer);
 }
-
-// 返回错误响应
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  if (err.name === 'UnauthorizedError') { 
-    // 验证错误  
-    res.status(401).send({ error: err.message });
-  } else if (err.code === 'SQLITE_ERROR') {
-    if (err.message.indexOf('no such table') !== -1) {
-      res.status(500).send({ error: '数据库结构尚未建立，请先执行扫描.'});
-    }
-  } else {
-    res.status(500).send({ error: err.message || err });
-  }
-});
-
-initApp().catch(err => console.error(err));
 
 let listenPort = 8888;
 if (config.listenPort) {
---
+++ socket.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const socket = require('socket.io');
+const jwtAuth = require('socketio-jwt-auth'); // 用于 JWT 验证的 socket.io 中间件
+const child_process = require('child_process'); // 子进程
+const { config } = require('./config');
+
+const initSocket = (server) => {
+  const io = socket(server);
+  if (config.auth) {
+    io.use(jwtAuth.authenticate({
+      secret: config.jwtsecret
+    }, (payload, done) => {
+      const user = {
+        name: payload.name,
+        group: payload.group
+      };
+
+      if (user.name === 'admin') {
+        done(null, user);
+      } else {
+        done(null, false, '只有 admin 账号能登录管理后台.');
+      }
+    }));
+  }
+
+  let scanner = null;
+
+  // 有新的客户端连接时触发
+  io.on('connection', function (socket) {
+    // console.log('connection');
+    socket.emit('success', {
+      message: '成功登录管理后台.',
+      user: socket.request.user,
+      auth: config.auth
+    });
+
+    // socket.on('disconnect', () => {
+    //   console.log('disconnect');
+    // });
+    
+    socket.on('ON_SCANNER_PAGE', () => {
+      if (scanner) {
+        // 防止用户在扫描过程中刷新页面
+        scanner.send({
+          emit: 'SCAN_INIT_STATE'
+        });
+      }
+    });
+
+    socket.on('PERFORM_SCAN', () => {
+      if (!scanner) {
+        scanner = child_process.fork(path.join(__dirname, './filesystem/scanner.js'), { silent: false }); // 子进程
+        scanner.on('exit', (code) => {
+          scanner = null;
+          if (code) {
+            io.emit('SCAN_ERROR');
+          }
+        });
+        
+        scanner.on('message', (m) => {
+          if (m.event) {
+            io.emit(m.event, m.payload);
+          }
+        });
+      }   
+    });
+
+    socket.on('KILL_SCAN_PROCESS', () => {
+      scanner.send({
+        exit: 1
+      });
+    });
+
+    // 发生错误时触发
+    socket.on('error', (err) => {
+      console.error(err);
+    });
+  });
+}
+
+module.exports = initSocket;
