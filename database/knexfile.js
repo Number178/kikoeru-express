e5b5d8d53ed75ab64fb96e61001aa7470b0b26cc
mason1900 <mw2010a@gmail.com>
2021-02-19 21:02:15 -0500

    Fix migration path to use absolute path
    Fix

--- database/knexfile.js
+++ database/knexfile.js
@@ -1,12 +1,10 @@
-// Update with your config settings.
+const path = require('path')
+const { config } = require('../config')
 
 module.exports = {
   client: 'sqlite3',
   connection: {
-    // 路径是相对于调用本文件的app.js
-    // 如果使用knex-migrate，需要修改为../sqlite/db.sqlite3
-    // 或手动指定
-    filename: './sqlite/db.sqlite3' 
+    filename: path.join(config.databaseFolderDir, 'db.sqlite3')
   },
   migrations: {
     tableName: 'knex_migrations'
