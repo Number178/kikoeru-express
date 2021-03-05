e1c453c21a002a6eb1d283f4bf551f9094ca10cf
mason1900 <mw2010a@gmail.com>
2021-03-05 16:57:44 -0500

    Remove DDL from review

--- database/db.js
+++ database/db.js
@@ -487,39 +487,6 @@ const deleteUserReview = (username, workid) => knex.transaction(trx => trx('t_re
 
 // TODO 写migration
 const getWorksWithReviews = ({username = '', limit = 1000, offset = 0, orderBy = 'release', sortOption = 'desc', filter} = {}) => knex.transaction(async(trx) => {
-  await trx.raw(
-    `CREATE VIEW IF NOT EXISTS userMetadata AS
-    SELECT t_work.id,
-      t_work.title,
-      json_object('id', t_work.circle_id, 'name', t_circle.name) AS circleObj,
-      t_work.release,
-      t_work.review_count,
-      t_work.dl_count,
-      t_work.nsfw,
-      userrate.userRating,
-      userrate.review_text,
-      userrate.progress,
-      userrate.updated_at,
-      json_object('vas', json_group_array(json_object('id', t_va.id, 'name', t_va.name))) AS vaObj,
-      userrate.user_name
-    FROM t_work
-    JOIN t_circle on t_circle.id = t_work.circle_id
-    JOIN r_va_work on r_va_work.work_id = t_work.id
-    join t_va on t_va.id = r_va_work.va_id
-    JOIN (
-        SELECT t_review.work_id,
-          t_review.rating AS userRating,
-          t_review.review_text,
-          t_review.progress,
-          strftime('%Y-%m-%d %H-%M-%S', t_review.updated_at, 'localtime') AS updated_at,
-          t_review.user_name
-        FROM t_review
-          JOIN t_work on t_work.id = t_review.work_id
-        ) AS userrate
-    ON userrate.work_id = t_work.id
-    GROUP BY t_work.id, userrate.user_name
-  `);
-  
   let works = [];
   let totalCount = 0;
   let query = () => trx('userMetadata').where('user_name', '=', username)
---
+++ database/migrations/20210305161906_remove_ddl_from_review_api.js
@@ -0,0 +1,45 @@
+exports.up = async function(knex) {
+  try {
+    await knex.schema.raw(
+      `CREATE VIEW IF NOT EXISTS userMetadata AS
+      SELECT t_work.id,
+        t_work.title,
+        json_object('id', t_work.circle_id, 'name', t_circle.name) AS circleObj,
+        t_work.release,
+        t_work.review_count,
+        t_work.dl_count,
+        t_work.nsfw,
+        userrate.userRating,
+        userrate.review_text,
+        userrate.progress,
+        userrate.updated_at,
+        json_object('vas', json_group_array(json_object('id', t_va.id, 'name', t_va.name))) AS vaObj,
+        userrate.user_name
+      FROM t_work
+      JOIN t_circle on t_circle.id = t_work.circle_id
+      JOIN r_va_work on r_va_work.work_id = t_work.id
+      join t_va on t_va.id = r_va_work.va_id
+      JOIN (
+          SELECT t_review.work_id,
+            t_review.rating AS userRating,
+            t_review.review_text,
+            t_review.progress,
+            strftime('%Y-%m-%d %H-%M-%S', t_review.updated_at, 'localtime') AS updated_at,
+            t_review.user_name
+          FROM t_review
+            JOIN t_work on t_work.id = t_review.work_id
+          ) AS userrate
+      ON userrate.work_id = t_work.id
+      GROUP BY t_work.id, userrate.user_name
+    `);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// Do nothing
+// eslint-disable-next-line no-unused-vars
+exports.down = async function(knex) {
+
+};
