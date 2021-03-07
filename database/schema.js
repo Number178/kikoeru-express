83bd57f990c4d3ca215d9cbb3ce0ee343b75b517
mason1900 <mw2010a@gmail.com>
2021-03-07 14:59:08 -0500

    refactor: add view staticMetadata to schema

--- database/schema.js
+++ database/schema.js
@@ -66,6 +66,39 @@ const createSchema = () => knex.schema
     table.foreign('work_id').references('id').inTable('t_work').onDelete('CASCADE'); // FOREIGN KEY 
     table.primary(['user_name', 'work_id']); // PRIMARY KEY
   })
+  .raw(`
+    CREATE VIEW IF NOT EXISTS staticMetadata AS
+    SELECT baseQueryWithVA.*,
+      json_object('tags', json_group_array(json_object('id', t_tag.id, 'name', t_tag.name))) AS tagObj
+    FROM (
+      SELECT baseQuery.*,
+        json_object('vas', json_group_array(json_object('id', t_va.id, 'name', t_va.name))) AS vaObj
+      FROM (
+        SELECT t_work.id, 
+          t_work.title,
+          t_work.circle_id,
+          t_circle.name,
+          json_object('id', t_work.circle_id, 'name', t_circle.name) AS circleObj,
+          t_work.nsfw,
+          t_work.release,
+          t_work.dl_count,
+          t_work.price,
+          t_work.review_count,
+          t_work.rate_count,
+          t_work.rate_average_2dp,
+          t_work.rate_count_detail,
+          t_work.rank
+        FROM t_work
+        JOIN t_circle ON t_circle.id = t_work.circle_id
+      ) AS baseQuery
+      JOIN r_va_work ON r_va_work.work_id = baseQuery.id
+      JOIN t_va ON t_va.id = r_va_work.va_id
+      GROUP BY baseQuery.id
+    ) AS baseQueryWithVA
+    LEFT JOIN r_tag_work ON r_tag_work.work_id = baseQueryWithVA.id
+    LEFT JOIN t_tag ON t_tag.id = r_tag_work.tag_id
+    GROUP BY baseQueryWithVA.id;
+  `)
   .then(() => {
     console.log(' * 成功构建数据库结构.');
   })
