0a94e4960d7e9aa58850e779230f0051d64babb6
mason1900 <mw2010a@gmail.com>
2021-03-22 17:20:39 -0400

    test: add test case for joinfragments()

--- test/urljoin.js
+++ test/urljoin.js
@@ -87,4 +87,15 @@ describe('joinfragments()', function(){
       'https://cdn.example.com/media/stream/VoiceWork/second/RJ290139%20%E3%80%90CV%EF%BC%9A%20%E4%B8%8A%E5%9D%82%E3%81%99%E3%81%BF%E3%82%8C%E3%80%91/Necogurashi%20ep01/mp3/01%20%E3%82%88%E3%81%86%E3%81%93%E3%81%9D%E7%8C%AB%E9%B3%B4%E9%A4%A8(%E3%81%AD%E3%81%93%E3%82%81%E3%81%84%E3%81%8B%E3%82%93)%E3%81%B8%E3%80%82%EF%BD%9E%E3%83%9F%E3%82%B1%E7%8C%AB%E3%81%AE%E5%A0%B4%E5%90%88%EF%BD%9E.mp3'
     )
   })
+
+  it('should return correct encoded offload URL for subdirectories on Windows', function() {
+    const baseUrl = 'https://cdn.example.com/media/stream/'
+    const rootFolderName = 'VoiceWork'
+    const workDir = 'second\\RJ290139 【CV： 上坂すみれ】'
+    const trackSubfolder = 'Necogurashi ep01\\mp3'
+    const trackTitle = '01 ようこそ猫鳴館(ねこめいかん)へ。～ミケ猫の場合～.mp3'
+    expect(joinFragments(baseUrl, rootFolderName, workDir, trackSubfolder, trackTitle)).to.equal(
+      'https://cdn.example.com/media/stream/VoiceWork/second/RJ290139%20%E3%80%90CV%EF%BC%9A%20%E4%B8%8A%E5%9D%82%E3%81%99%E3%81%BF%E3%82%8C%E3%80%91/Necogurashi%20ep01/mp3/01%20%E3%82%88%E3%81%86%E3%81%93%E3%81%9D%E7%8C%AB%E9%B3%B4%E9%A4%A8(%E3%81%AD%E3%81%93%E3%82%81%E3%81%84%E3%81%8B%E3%82%93)%E3%81%B8%E3%80%82%EF%BD%9E%E3%83%9F%E3%82%B1%E7%8C%AB%E3%81%AE%E5%A0%B4%E5%90%88%EF%BD%9E.mp3'
+    )
+  })
 })
