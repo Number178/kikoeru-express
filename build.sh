f678320717e3026c8eb153e15f91403a5d53fd3a
kahosan <15116355657l@gmail.com>
2022-12-17 22:42:55 +0800

    feat: add docker script

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build -t kikoeru-backend:v1 .
+docker tag kikoeru-backend:v1 custom/kikoeru-backend:v1
---
+++ remove.sh
@@ -0,0 +1,3 @@
+docker rm kikoeru_kikoeru-custom_1
+docker image rm custom/kikoeru-backend:v1
+docker image rm kikoeru-backend:v1
